cae2d41d394abf8b4897fec366b2c2798c5848d1
VladimirNeto <pkm_ik204@mail.ru>
2022-06-12 12:34:48 +0400

    One commit diplomq

---
+++ src/main/java/TodoServer.java
@@ -0,0 +1,43 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TodoServer {
+    int port;
+    Todos todos;
+
+    public TodoServer(int port, Todos todos) {
+        this.port = port;
+        this.todos = todos;
+    }
+
+    public void start() {
+        System.out.println("Starting server at " + port + "...");
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
+                ) {
+                    String inTask = in.readLine();
+                    JsonObject jsonObject = JsonParser.parseString(inTask).getAsJsonObject();
+                    String type = jsonObject.get("type").getAsString();
+                    String task = jsonObject.get("task").getAsString();
+                    if (type.equals("ADD")) {
+                        todos.addTask(task);
+                    } else if (type.equals("REMOVE")) {
+                        todos.removeTask(task);
+                    }
+                    out.println(todos.getAllTasks());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Todos.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Todos {
+    protected List<String> listTask = new ArrayList<>();
+
+    public void addTask(String task) {
+        listTask.add(task);
+    }
+
+    public void removeTask(String task) {
+        listTask.remove(task);
+    }
+
+    public String getAllTasks() {
+        Collections.sort(listTask);
+        StringBuilder list = new StringBuilder();
+        for (String task : listTask) {
+            list.append(task).append(" ");
+        }
+        return list.toString();
+    }
+}
